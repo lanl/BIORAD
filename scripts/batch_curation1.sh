5904f42108456ecc6c860d077a078874826bdb24
Jason Gans <36452131+jgans@users.noreply.github.com>
2024-12-18 18:01:57 -0700

    Update batch_curation1.sh

--- scripts/batch_curation1.sh
+++ scripts/batch_curation1.sh
@@ -1,23 +1,17 @@
 #!/bin/sh
 #
-# Perform machine learning tests for the data curation paper
+# Perform machine learning tests for the data curation manuscript
 # All clustering performed using the "--cluster.align" flag (first time only)
 
 NUM_TRIAL=100
 
 for TARGET in all pdbbind units no_kd different_heterodimer
 do
-    # Note that random forest predictions are pulled to the mean of the regression output
-    ../src/biorad -t ${NUM_TRIAL} --affinity data/curation_${TARGET}.csv \
-        --pdb.ignore-charge --pdb ../data/PDBBind_hydrogen \
+    src/biorad -t ${NUM_TRIAL} --affinity data/curation_${TARGET}.csv \
+        --pdb.ignore-charge --pdb heterodimer_pdb_hydrogen \
         --cluster cluster/cluster.align.${TARGET} \
-        -o output_hetatm/curation_${TARGET}.out \
+        -o output/curation_${TARGET}.out \
         --hist.min 0.0 \
         --hist.max 12.0 \
         --hist.bins 1
-        
-        # Adjacency features
-        #--hist.min 0.1 \
-        #--hist.max 20.0 \
-        #--hist.bins 25
 done
