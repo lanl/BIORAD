99e6460fcc7c75ef4ca04becb47a8536c180f76a
Jason Gans <36452131+jgans@users.noreply.github.com>
2024-12-18 18:12:35 -0700

    Update batch_curation1.sh

--- scripts/batch_curation1.sh
+++ scripts/batch_curation1.sh
@@ -3,6 +3,9 @@
 # Perform machine learning tests for the data curation manuscript
 # All clustering performed using the "--cluster.align" flag (first time only)
 
+# Create the output directory if it does not already exist
+mkdir -p output
+
 NUM_TRIAL=100
 
 for TARGET in all pdbbind units no_kd different_heterodimer
